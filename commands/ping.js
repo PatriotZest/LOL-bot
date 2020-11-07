7b7b2e3b0c26c708d678de2c4992a5aa738cba41
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2020-11-07 16:05:23 +0530

    Lol bot

---
+++ commands/ping.js
@@ -0,0 +1,17 @@
+﻿  
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "ping",
+    description: "test command",
+
+    async run (client, message, args) {
+
+
+        const ping = new Discord.MessageEmbed()
+        .setDescription(`🏓\`${client.ws.ping}\`ms`);
+
+
+        message.channel.send(ping);
+    }
+}
