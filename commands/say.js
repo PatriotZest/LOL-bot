8755a0a26505c346d42e26f92ac1d3e9d0e4f438
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2020-11-09 11:03:33 +0530

    toString()

--- commands/say.js
+++ commands/say.js
@@ -5,7 +5,7 @@ module.exports = {
     description: "says the command from bot",
      async run(client, message , args){
      function emoji (id) {
-        return client.emojis.cache.get(id).toString
+        return client.emojis.cache.get(id).toString()
     }
      if(!message.member.roles.cache.has("775226834685919252")) return message.channel.send('You are not the Great Patriot so you cannot use this command.' + emoji('766605727392727062'))
      const sayMessage = args.join(" ");
