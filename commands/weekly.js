8a4dd2ed04d72b34a58cc89e0fe5133e8af78461
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2020-11-09 09:55:56 +0530

    weekly command

---
+++ commands/weekly.js
@@ -0,0 +1,26 @@
+const db = require('quick.db');
+const ms = require('parse-ms');
+
+module.exports = { 
+    name: "weekly",
+    description: "Receive a weekly award of money",
+
+    async run (client, message, args) {
+        let user = message.author;
+        let timeout = 86400000; 
+        let amount = 25000;
+
+        let weekly = await db.fetch(`weekly_${message.guild.id}_${user.id}`);
+
+        if(weekly !== null && timeout - (Date.now() - weekly) > 0){
+            let time = ms(timeout - (Date.now() - weekly));
+
+            return message.channel.send(`You've already collected your weekly award. Come back in ${time.days}d, ${time.hours}h, ${time.minutes}m, and ${time.seconds}s`)
+        } else {
+            db.add(`money_${message.guild.id}_${user.id}`, amount);
+            db.set(`weekly_${message.guild.id}_${user.id}`, Date.now());
+
+            message.channel.send(`Successfully added ${amount} coins to your account`)
+        }
+    }
+}
