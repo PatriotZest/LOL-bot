906c68e2e8ba62631ee7387472cdc58bfa3f8f79
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2020-11-10 08:31:15 +0530

    say fix

---
+++ commands/ticket.js
@@ -0,0 +1,19 @@
+﻿const channelId = '775302318564442132'
+const check = '✅'
+module.exports{
+    commands: ['ticket','support'],
+    minArgs: 1,
+    expectedArgs: '<message>',
+    callback: (userMessage, arguments, text) =>{
+        const {guild,member} = userMessage
+        const channel = guild.channels.cache.get(channelId)
+        message.channel.send(`A new ticket has been created by <@${member.id}>
+        
+        "${text}"
+        
+        Click the ${check} icon when finished :)`)
+        .then(textMessage =>{
+            message.react(check)
+        })
+    },
+}
