c5871ecc310b72fa17d49c48d08a4ec0ef776aba
PatriotZest <49161627+PatriotZest@users.noreply.github.com>
2020-11-10 08:35:49 +0530

    say fix again

--- commands/say.js
+++ commands/say.js
@@ -4,7 +4,6 @@ module.exports = {
     name: 'say',
     description: "says the command from bot",
      async run(client, message , args){
-     if(!message.member.roles.cache.has("775226834685919252")) return message.channel.send('You are not the Great Patriot so you cannot use this command.')
      const sayMessage = args.join(" ");
      message.delete().catch(err => console.log(err));
      message.channel.send(sayMessage);
